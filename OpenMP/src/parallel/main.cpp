e467bdc9f6ad0d6d0778a939a966a84634a84833
Gabriel Pereira <gabrielantoniopereira85@gmail.com>
2025-09-24 21:54:02 -0300

    measuring execution time

--- OpenMP/src/parallel/main.cpp
+++ OpenMP/src/parallel/main.cpp
@@ -5,16 +5,24 @@
 
 #include <iostream>
 #include <omp.h>
+#include <chrono>
 #include "state.h"
 #include "sim_anneal.h"
 
 using namespace std;
 
 int main() {
+    auto start = std::chrono::high_resolution_clock::now();
     pair<double, state> res = simAnneal();
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double, std::milli> elapsed = end - start;
+    
     double E_best = res.first;
     state best = res.second;
 
+    cout << "Execution time: " << elapsed.count() << " ms\n";
+    
     cout << "Length of shortest path found: " << E_best << "\n";
     cout << "Order of points in shortest path:\n";
     for(auto x : best.points) {
--- OpenMP/src/standard/main.cpp
+++ OpenMP/src/standard/main.cpp
@@ -4,15 +4,23 @@
  */
 
 #include <iostream>
+#include <chrono>
 #include "state.h"
 #include "sim_anneal.h"
 
 using namespace std;
 
 int main() {
+    auto start = std::chrono::high_resolution_clock::now();
     pair<double, state> res = simAnneal();
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double, std::milli> elapsed = end - start;
+    
     double E_best = res.first;
     state best = res.second;
+
+    cout << "Execution time: " << elapsed.count() << " ms\n";
 
     cout << "Length of shortest path found: " << E_best << "\n";
     cout << "Order of points in shortest path:\n";
